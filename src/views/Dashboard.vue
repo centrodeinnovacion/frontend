5ac48e168d51123fb364f300fa4489868f0395e8
Mesi Rendon <amrendonsa@unal.edu.co>
2018-10-12 16:36:22 -0500

    Adding buttons and dashboard
            I've added a new view called Dashboard to hold the buttons
            defined by Angelica.

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <form @submit.prevent="">
+      <input type="file" placeholder="Drag a file to upload" name="upload" id="upload">
+      <input type="file" placeholder="Drag a file to verify" name="verify" id="verify">
+      <button type="button" class="btn btn-primary">Download</button>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Dashboard'
+  }
+</script>
+
+<style scoped>
+
+</style>
