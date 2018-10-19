5f5d6d4f9e143ac60011eb8bc90fb12340613bf9
Nick Castro <nycastrog@unal.edu.co>
2018-10-19 15:56:06 -0500

    Styles of donwload page

---
+++ src/views/Download.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="dashboard">
+    <div class="row">
+      <div class="col-3">
+      <h2>Instrucciones</h2>
+    <p>Suponga que usted quiere cargar, verificar y descargar el registro sanitario o médico de un producto, para esto seleccione una de las siguientes acciones: </p>
+
+    <form @submit.prevent="">
+      <div class="buttonscol">
+      <input type="file"  placeholder="Drag a file to upload" name="upload" id="upload" class="inputstylebtn"  >
+      <input type="file" placeholder="Drag a file to verify" name="verify" id="verify" class="inputstylebtnverify">
+      <button type="button" class="btn btn-dashboard">Descargar documento <i class="fas fa-cloud-download-alt"></i> </button>
+      </div>    </form>  </div>
+
+    <div class="col-6">
+
+        <p>Escriba el HASH del documento que desea descargar</p>
+        <input type= “text” class="box-download">
+        <button type="button" class="btnpink "> Descargar <i class="fas fa-cloud-download-alt"></i> </button>
+
+    </div>
+
+
+
+    </div>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: 'Dashboard'
+  }
+</script>
+
+<style scoped>
+
+</style>
