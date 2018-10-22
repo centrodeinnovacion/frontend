0f0caba1d94c9a869e1ae39c54653fc1578cd3a5
Angelica Paeea <jeaparrave@unal.edu.co>
2018-10-22 14:07:51 -0500

    Create upload, and xxx components

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,31 +1,35 @@
 <template>
-  <div class="dashboard">
-    <div class="row">
-      <div class="col-3">
-      <h2>Instrucciones</h2>
-    <p>Suponga que usted quiere cargar, verificar y descargar el registro sanitario o médico de un producto, para esto seleccione una de las siguientes acciones: </p>
+    <div class="dashboard">
+        <div class="row">
+            <div class="col-lg-2 col-md-3 col-sm-3 col-xs-12">
+               <buttons></buttons>
+            </div>
 
-    <form @submit.prevent="">
-      <div class="buttonscol">
-      <input type="file"  placeholder="Drag a file to upload" name="upload" id="upload" class="inputstylebtn"  >
-      <input type="file" placeholder="Drag a file to verify" name="verify" id="verify" class="inputstylebtnverify">
-      <button type="button" class="btn btn-dashboard">Descargar documento <i class="fas fa-cloud-download-alt"></i> </button>
-      </div>    </form>  </div>
+            <div class="col-lg-7 col-md-6 col-sm-6 col-xs-12">
+                <xxx></xxx>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12">
+                <upload></upload>
+            </div>
 
-    <div class="col-6">
+
+        </div>
     </div>
-
-
-
-    </div>
-  </div>
 
 </template>
 
 <script>
-  export default {
-    name: 'Dashboard'
-  }
+    import Buttons from '@/components/menus/Buttons'
+    import xxx from '@/components/common/xxx'
+    import Upload from '@/components/common/Upload'
+    export default {
+        name: 'Dashboard',
+        components: {
+            Buttons,
+            xxx,
+            Upload
+        }
+    }
 </script>
 
 <style scoped>
