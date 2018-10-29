327bb973a7c0e7b8f03a52f7bda3ed6527691cd8
Anni Piragauta <aapiragautau@unal.edu.co>
2018-10-29 12:49:50 -0500

    Integrating Toolkit API to the project
    	We've added the upload file functionality from the Toolkit
    	API, also we added form-data to send the file to the post
    	request to the API.
    
    	We've made modifications on the Buttons component in order
    	to upload the file.
    
    	We've also modificated the Upload component to show the API
    	response into the frontend.
    
    	Reported-by: Mesi Rendon

--- src/components/menus/Buttons.vue
+++ src/components/menus/Buttons.vue
@@ -1,26 +1,45 @@
 <template>
-   <div>
-       <h2>Instrucciones</h2>
-       <p>Suponga que usted quiere cargar, verificar y descargar el registro sanitario o médico de un producto,
-           para esto seleccione una de las siguientes acciones: </p>
+  <div>
+    <h2>Instrucciones</h2>
+    <p>Suponga que usted quiere cargar, verificar y descargar el registro sanitario o médico de un producto,
+      para esto seleccione una de las siguientes acciones: </p>
 
-       <form @submit.prevent="">
-           <div class="buttonscol">
-               <input type="file" placeholder="Drag a file to upload" name="upload" id="upload"
-                      class="inputstylebtn">
-               <input type="file" placeholder="Drag a file to verify" name="verify" id="verify"
-                      class="inputstylebtnverify">
-               <button type="button" class="btn btn-dashboard">Descargar documento <i
-                       class="fas fa-cloud-download-alt"></i></button>
-           </div>
-       </form>
-   </div>
+    <form @submit.prevent>
+      <div class="buttonscol">
+        <input type="file" placeholder="Drag a file to upload" name="upload" id="upload"
+               class="btn btn-dashboard" @change="upload">
+        <input type="file" placeholder="Drag a file to verify" name="verify" id="verify"
+               class="inputstylebtnverify">
+        <button type="button" class="btn btn-dashboard">Descargar documento <i
+                class="fas fa-cloud-download-alt"></i></button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Buttons"
+  import {mapActions, mapMutations} from 'vuex'
+  import * as constants from '@/store/constants'
+
+  export default {
+    name: 'Buttons',
+    methods: {
+      ...mapActions({
+        uploadFile: constants.TOOLKIT_UPLOAD_FILE
+      }),
+      ...mapMutations({
+        setProperty: constants.TOOLKIT_SET_PROPERTY
+      }),
+      upload(e){
+        const files = e.target.files
+        if(!files.length){ return }
+        this.setProperty({hash: {hash: 'procesando...', tx: 'procesando...'}})
+        const file = files[0]
+        console.log(file)
+        this.uploadFile(file)
+      }
     }
+  }
 </script>
 
 <style scoped>
