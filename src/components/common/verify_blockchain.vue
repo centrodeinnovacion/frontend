bf9d3331665b06c9d2b2fb69d66f52c759e8e6cc
AngelicaP <jeaparrave@unal.edu.co>
2018-10-31 13:12:34 -0500

    animation and styles for "searching the file in blockchain"

--- src/components/common/verify_blockchain.vue
+++ src/components/common/verify_blockchain.vue
@@ -1,7 +1,8 @@
 <template>
-    <div class="mt-5">
-        <h3 class="colorgreen">Verificando existencia del documento</h3><p> El archivo es buscado en la red de Blockchain.</p>
-        <button class="btndiamond"><span class="buttontextdiamond"><i class="fas fa-arrow-right"></i></span></button><p>Clic para continuar</p>
+    <div class="explanation">
+        <i class="color_lightgreen fas fa-search"></i>
+        <p class="explanation-text mt-5 saving">Buscando el archivo en la red de Blockchain</p>
+            <p class="explanation-text saving"><span>.</span><span>.</span><span>.</span></p>
     </div>
 
 </template>
