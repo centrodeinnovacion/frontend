a6fe5696070d182e3b14e85e435c8c8fe8f656cc
Angelica Parra <jeaparrave@unal.edu.co>
2018-10-31 20:02:21 -0500

    File not found and File found animation. Searching document in ifs animation

--- src/components/common/verify_blockchain.vue
+++ src/components/common/verify_blockchain.vue
@@ -4,7 +4,6 @@
         <p class="explanation-text mt-5 saving">Buscando el archivo en la red de Blockchain</p>
             <p class="explanation-text saving"><span>.</span><span>.</span><span>.</span></p>
     </div>
-
 </template>
 
 <script>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,44 +1,54 @@
 <template>
-    <div class="dashboard">
-        <div class="row">
-            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
-                <buttons></buttons>
-            </div>
-            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                <global></global>
-                <verify></verify>
-            </div>
-            <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12">
-                <verify_blockchain></verify_blockchain>
-            </div>
-        </div>
-        <div class="row download">
-            <div>
-                <download></download>
-            </div>
-        </div>
+  <div class="dashboard">
+    <div class="row">
+      <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+        <buttons></buttons>
+      </div>
+      <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+        <global></global>
+        <searchipfs></searchipfs>
+        <!--<verify></verify>-->
+      </div>
+      <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12">
+        <!--<verify_blockchain></verify_blockchain>-->
+        <!--<filenofound></filenofound>-->
+        <filefound></filefound>
+
+      </div>
     </div>
+    <div class="row download">
+      <div>
+        <download></download>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-    import Buttons from '@/components/menus/Buttons'
-    import Global from '@/components/common/Global'
-    import Upload from '@/components/common/Upload'
-    import Download from '@/components/common/Download'
-    import Uploadipfs from '@/components/common/Uploadipfs'
-    import verify from '@/components/common/verify'
-    import verify_blockchain from '@/components/common/verify_blockchain'
+  import Buttons from '@/components/menus/Buttons'
+  import Global from '@/components/common/Global'
+  import Upload from '@/components/common/Upload'
+  import Download from '@/components/common/Download'
+  import Uploadipfs from '@/components/common/Uploadipfs'
+  import verify from '@/components/common/verify'
+  import verify_blockchain from '@/components/common/verify_blockchain'
+  import Searchipfs from '@/components/common/Searchipfs'
+  import Filenofound from '@/components/common/Filenofound'
+  import Filefound from '@/components/common/Filefound'
 
-    export default {
-        name: 'Dashboard',
-        components: {
-            Buttons,
-            Global,
-            Upload,
-            Uploadipfs,
-            Download,
-            verify,
-            verify_blockchain
-        }
+  export default {
+    name: 'Dashboard',
+    components: {
+      Buttons,
+      Global,
+      Upload,
+      Uploadipfs,
+      Download,
+      verify,
+      verify_blockchain,
+      Searchipfs,
+      Filenofound,
+      Filefound
     }
+  }
 </script>
