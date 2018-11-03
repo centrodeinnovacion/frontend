747d133e9d37cad7ed8864cd92ca3c2a15e5a964
AngelicaP <jeaparrave@unal.edu.co>
2018-11-03 11:40:50 -0500

    I changed the name of the file "Verify" for "Searchbc" /seach in blockchain)

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -6,8 +6,8 @@
       </div>
       <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
         <global></global>
-        <searchipfs></searchipfs>
-        <!--<verify></verify>-->
+        <!--<searchipfs></searchipfs>-->
+        <verify></verify>
       </div>
       <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12">
         <div class="explanation">
