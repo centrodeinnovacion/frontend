14a61576af953f2651c031448a74c2e469204fc7
AngelicaP <jeaparrave@unal.edu.co>
2018-11-05 00:14:47 -0500

    Animation: File no found in ipfs, and Seacrhing file in ipfs

--- src/components/common/Notfoundipfs.vue
+++ src/components/common/Notfoundipfs.vue
@@ -1,20 +1,21 @@
 <template>
-  <div class="verify mx-auto fixed-top ">
-    <div class="hash3 fifthash">
-      ...<i class="fas fa-thumbs-up"></i></div>
-    <div class="nofilefound-5">No se encontró el </br>archivo en el nodo</div>
-    <div class="nofilefound-6">No se encontró el </br>archivo en el nodo</div>
-    <div class="line1 mx-auto fixed-top">
+    
+</template><template>
+    <div class="verify mx-auto fixed-top ">
+        <div class="nofilefound-7">No se encontró el </br>archivo en el nodo</div>
+        <div class="nofilefound-5">No se encontró el </br>archivo en el nodo</div>
+        <div class="nofilefound-6">No se encontró el </br>archivo en el nodo</div>
+        <div class="line1 mx-auto fixed-top">
 
-      <svg width="500px" height="550px"  viewBox="0 120 650 450" enable-background="new 0 0 340 333" xml:space="preserve">
+            <svg width="500px" height="550px"  viewBox="0 120 650 450" enable-background="new 0 0 340 333" xml:space="preserve">
         <!--doc azul-->
         <path class="st3" d="M237.4,667.2v-7.5h-11.1c-0.7,0-1.3,0.6-1.3,1.3v25.7c0,0.7,0.6,1.3,1.3,1.3h18.6c0.7,0,1.3-0.6,1.3-1.3v-18.2 h-7.5C238,668.5,237.4,667.9,237.4,667.2z M240.9,680.2c0,0.4-0.3,0.7-0.7,0.7H231c-0.4,0-0.7-0.3-0.7-0.7v-0.4 c0-0.4,0.3-0.7,0.7-0.7h9.3c0.4,0,0.7,0.3,0.7,0.7V680.2z M240.9,676.7c0,0.4-0.3,0.7-0.7,0.7H231c-0.4,0-0.7-0.3-0.7-0.7v-0.4 c0-0.4,0.3-0.7,0.7-0.7h9.3c0.4,0,0.7,0.3,0.7,0.7V676.7z M240.9,672.7v0.4c0,0.4-0.3,0.7-0.7,0.7H231c-0.4,0-0.7-0.3-0.7-0.7v-0.4 c0-0.4,0.3-0.7,0.7-0.7h9.3C240.6,672.1,240.9,672.4,240.9,672.7z M246.3,666.4v0.3h-7.1v-7.1h0.3c0.4,0,0.7,0.1,0.9,0.4l5.4,5.4 C246.1,665.7,246.3,666.1,246.3,666.4z"/>
-        <!--linea 1-->
+                <!--linea 1-->
         <g>
         <path class="st0 path ad-4" d="M237.5,663.5c0,0-65-48,41-84"/>
         <ellipse transform="matrix(1.314393e-02 -0.9999 0.9999 1.314393e-02 -294.7224 857.3788)" class="st1 ad-4 op-0" cx="287" cy="572" rx="5" ry="5"/>
         </g>
-        <!--doc1-->
+                <!--doc1-->
         <svg class="bounceinslow align-self-center ade-1"x="238px" y="483" width="120.5px" height="90px" viewBox="0 -25 68.5 98" style="enable-background:new 0 0 54.5 58;"xml:space="preserve">
         <g class="blink"><path class="st4" d="M36.5,17.3H26.5c-0.7,0-1.3,0.6-1.3,1.3v25.7c0,0.7,0.6,1.3,1.3,1.3h18.6c0.7,0,1.3-0.6,1.3-1.3V27.2"/>
           <g><path class="st5" d="M39.4,28.7l0.3,0.3c0.3,0.3,0.3,0.7,0,0.9l-6.6,6.6c-0.3,0.3-0.7,0.3-0.9,0l-0.3-0.3c-0.3-0.3-0.3-0.7,0-0.9 l6.6-6.6C38.7,28.4,39.1,28.4,39.4,28.7z"/>
@@ -26,11 +27,11 @@
           <line class="st7" x1="14.1" y1="31.2" x2="8.6" y2="30.7"/><line class="st7" x1="35.6" y1="10.2" x2="35.6" y2="3.2"/><line class="st7" x1="35.6" y1="60.2" x2="35.6" y2="53.2"/>
         </g>
         </svg>
-        <!--linea 2-->
+                <!--linea 2-->
         <path class="st0 path ade-4 ad-5" d="M278.5,490.5c0,0-449-250-180-206"/>
         <ellipse transform="matrix(1.314384e-02 -0.9999 0.9999 1.314384e-02 -183.3162 382.2591)" class="st1 ade-4 ad-5 op-0" cx="102" cy="284" rx="5" ry="5"/>
         <ellipse transform="matrix(1.314393e-02 -0.9999 0.9999 1.314393e-02 -211.6773 767.5226)" class="st1 ade-4 ad-5 op-0" cx="283" cy="491" rx="5" ry="5"/>
-        <!--doc2-->
+                <!--doc2-->
         <svg class="bounceinslow align-self-center"x="79px" y="220" width="120.5px" height="90px" viewBox="0 -25 68.5 98" style="enable-background:new 0 0 54.5 58;"xml:space="preserve">
           <g class="blink ade-5"><path class="st4" d="M36.5,17.3H26.5c-0.7,0-1.3,0.6-1.3,1.3v25.7c0,0.7,0.6,1.3,1.3,1.3h18.6c0.7,0,1.3-0.6,1.3-1.3V27.2"/>
             <g><path class="st5" d="M39.4,28.7l0.3,0.3c0.3,0.3,0.3,0.7,0,0.9l-6.6,6.6c-0.3,0.3-0.7,0.3-0.9,0l-0.3-0.3c-0.3-0.3-0.3-0.7,0-0.9 l6.6-6.6C38.7,28.4,39.1,28.4,39.4,28.7z"/>
@@ -42,21 +43,33 @@
           <line class="st7" x1="14.1" y1="31.2" x2="8.6" y2="30.7"/><line class="st7" x1="35.6" y1="10.2" x2="35.6" y2="3.2"/><line class="st7" x1="35.6" y1="60.2" x2="35.6" y2="53.2"/>
         </g>
         </svg>
-        <!--linea 3-->
+                <!--linea 3-->
         <path class="st0 path ade-9 ad-4" d="M174,239c0,0,86.5-244.5,230.5-146.5"/>
         <ellipse transform="matrix(1.314384e-02 -0.9999 0.9999 1.314384e-02 310.6322 500.7431)" class="st1 ade-9 ad-4 op-0" cx="409" cy="93" rx="5" ry="5"/>
         <ellipse transform="matrix(1.314384e-02 -0.9999 0.9999 1.314384e-02 -67.2664 409.8444)" class="st1 ade-9 ad-4 op-0" cx="174" cy="239" rx="5" ry="5"/>
-      </svg>
+                <!--doc3-->
+        <svg class="bounceinslow align-self-center"x="375px" y="40" width="120.5px" height="90px" viewBox="0 -25 68.5 98" style="enable-background:new 0 0 54.5 58;"xml:space="preserve">
+          <g class="blink ade-9"><path class="st4" d="M36.5,17.3H26.5c-0.7,0-1.3,0.6-1.3,1.3v25.7c0,0.7,0.6,1.3,1.3,1.3h18.6c0.7,0,1.3-0.6,1.3-1.3V27.2"/>
+            <g><path class="st5" d="M39.4,28.7l0.3,0.3c0.3,0.3,0.3,0.7,0,0.9l-6.6,6.6c-0.3,0.3-0.7,0.3-0.9,0l-0.3-0.3c-0.3-0.3-0.3-0.7,0-0.9 l6.6-6.6C38.7,28.4,39.1,28.4,39.4,28.7z"/>
+              <path class="st5" d="M38.4,36.5l-6.6-6.6c-0.3-0.3-0.3-0.7,0-0.9l0.3-0.3c0.3-0.3,0.7-0.3,0.9,0l6.6,6.6c0.3,0.3,0.3,0.7,0,0.9 l-0.3,0.3C39.1,36.8,38.7,36.8,38.4,36.5z"/>
+              <path class="st3" d="M46.4,24.1v0.3h-7.1v-7.1h0.3c0.4,0,0.7,0.1,0.9,0.4l5.4,5.4C46.3,23.4,46.4,23.7,46.4,24.1z"/></g>
+          </g>
+        <g class="animationnotfound ade-9"><line class="st7" x1="54.6" y1="16.2" x2="58.1" y2="12.7"/><line class="st7" x1="56.6" y1="46.7" x2="60.1" y2="50.2"/>
+          <line class="st7" x1="56.6" y1="31.2" x2="62.1" y2="30.7"/><line class="st7" x1="16.1" y1="16.2" x2="12.6" y2="12.7"/><line class="st7" x1="14.1" y1="46.7" x2="10.6" y2="50.2"/>
+          <line class="st7" x1="14.1" y1="31.2" x2="8.6" y2="30.7"/><line class="st7" x1="35.6" y1="10.2" x2="35.6" y2="3.2"/><line class="st7" x1="35.6" y1="60.2" x2="35.6" y2="53.2"/>
+        </g>
+        </svg>
+            </svg>
+        </div>
+        <div class="hashbig"><p class="linebottom">HASH</p>
+            <h5 class="coloryellow">0x7ba87aac76d29f20cefa161dabe300039d569b3cb5a1acb768af84c1bd063ff3</h5></div>
     </div>
-    <div class="hashbig"><p class="linebottom">HASH</p>
-      <h5 class="coloryellow">0x7ba87aac76d29f20cefa161dabe300039d569b3cb5a1acb768af84c1bd063ff3</h5></div>
-  </div>
 </template>
 
 <script>
-  export default {
-    name: "Searchipfs"
-  }
+    export default {
+        name: "Notfoundipfs"
+    }
 </script>
 
 <style scoped>
