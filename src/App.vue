cd249056aca970b5d081fee039725447d0573160
AngelicaP <jeaparrave@unal.edu.co>
2018-11-13 18:52:38 -0500

    footer

--- src/App.vue
+++ src/App.vue
@@ -4,23 +4,22 @@
     <router-view/>
     <footer class="">
       <div class="d-flex fixed-bottom" v-if="route !== 'home' && route !== 'about'">
-        <div class="p-2 ml-5 mr-auto mb-3">
-          <button type="button"  class="btnDiamond" data-toggle="modal" data-target="#tutorialModal" id="Tutorial"><i class="fas fa-question"></i></button>
-          <p class="pl-4 mt-3">¿Cómo usar este toolkit?</p>
+        <div class="p-2 ml-5 mr-auto mb-0 pb-0" data-toggle="tooltip" data-placement="top" title="¿Cómo usar este tooltip?">
+          <button type="button"  class="btnDiamond" data-toggle="modal" data-target="#tutorialModal" id="Tutorial" ><i class="fas fa-question"></i></button>
         </div>
         <div class="p-2 mr-3 mb-3">
           <router-link class="btnDiamond" :to="{name: back}"><i class="fas fa-arrow-left"></i>
           </router-link>
         </div>
-        <div class="p-2 mr-3 mb-3">
+        <div class="p-2 mr-3 mb-0">
           <router-link class="btnDiamond" :to="{name: forward}"><i class="fas fa-arrow-right"></i>
           </router-link>
         </div>
       </div>
-      <div class="footer-copyright text-center py-3" v-if="route !== 'dashboard' && route !== 'home'">© 2018 Copyright.
+      <div class="footer-copyright text-center py-3" v-if="route !== 'dashboard' && route !== 'home'">© 2018.
         <a href=""> Términos y condiciones de uso.</a>
       </div>
-      <div class="d-flex fixed-bottom footer-copyright justify-content-center align-content-center text-center py-3" v-if="route !== 'howitworks' && route !== 'about'">© 2018 Copyright.
+      <div class="d-flex fixed-bottom footer-copyright justify-content-center align-content-center text-center py-3" v-if="route !== 'howitworks' && route !== 'about'">© 2018.
         <a href=""> Términos y condiciones de uso.</a>
       </div>
     </footer>
