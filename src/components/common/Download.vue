991bd2459105098e8792bae6e5856444975fc1da
AngelicaP <jeaparrave@unal.edu.co>
2018-11-12 22:09:42 -0500

    Update src/components/common/Download.vue

--- src/components/common/Download.vue
+++ src/components/common/Download.vue
@@ -6,7 +6,7 @@
           <div class="modal-body">
             <p class="textcenter">Escriba el HASH del documento que desea descargar</p>
             <input type="text" class="box-download" placeholder="IPFS hash" v-model="hash">
-            <div class="d-flex justify-content-center mt-4">
+            <div class="d-flex justify-content-center pt-5">
               <button type="button" class="btnpink" @click="getFile"> Descargar
                 <i class="iconbutton mt-4 pt-2 pl-3 pr-2 icon-download"></i>
               </button>
