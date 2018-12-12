637a1ff8aa1bbffb187ed0d3e005a621cdb6396e
AngelicaP <jeaparrave@unal.edu.co>
2018-12-12 10:47:34 -0500

    Update UploadBlockchain.vue

--- src/components/common/UploadBlockchain.vue
+++ src/components/common/UploadBlockchain.vue
@@ -6,10 +6,10 @@
     <div class="pl-6">
       <h3 class="colorgreen">Transacción de IPFS a Blockchain</h3>
       <p> El hash del documento es transferido de <a href="https://ipfs.io/" target="_blank">IPFS</a> a nodos de la red de
-      Blockchain, a través de este Kit. Esta transferencia es identificada con otro hash.</p>
-      <h4 class="colorgreen">Hash de la transacción:</h4>
+      Blockchain, a través de este Kit.</p>
+      <h4 class="colorgreen">Hash de La transacción:</h4>
       <textarea readonly class="colorgreen" id="etheHash" @click="doCopy">{{this.hash.tx.substring(0,2) === '0x' || hash.tx === 'procesando...' || hash.tx === null ? hash.tx : `El archivo "${hash.tx}" ya fue subido previamente`}}</textarea>
-      <p class="mb-5 pb-5">Haga clic en el hash para ir a Ropsten en donde se muestra que efectivamente la transacción se ha realizado.</p>
+      <p class="mb-5 pb-5">Haga clic en el hash para ir a Ropsten en donde podrá verificar que la transacción se ha realizado.</p>
       <p></p>
     </div>
   </div>
