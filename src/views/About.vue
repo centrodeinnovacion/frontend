c7008a5b03982c3d5a350d9f96d5bbd218f85f3c
AngelicaP <jeaparrave@unal.edu.co>
2018-12-18 14:43:27 -0500

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,7 +3,7 @@
     <div class="More d-flex justify-content-center">
       <div class="contentMore">
         <h2>¿Quiere saber más?</h2>
-        <p>Colsulte la documentación para desarrolladores <a
+        <p>Consulte la documentación para desarrolladores <a
             href="https://github.com/centrodeinnovacion/api#ict-ministrys-digital-innovation-center-blockchain-toolkit-api"
             target="_blank">aquí</a>.</p>
         <router-link class="btnDiamond" :to="{name: 'dashboard'}"><i class="fas fa-arrow-left"></i></router-link>
